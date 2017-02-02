0254f42effe7c518f042d1933ca588c7708cb6d6
Guy Daher <guy.daher@algolia.com>
2017-02-02 16:31:03 +0100

    Quick successful test of InstantSearchCore with BestBuy_promo Index.

--- ecommerce/ecommerce/ViewController.swift
+++ ecommerce/ecommerce/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import InstantSearchCore
+import AlgoliaSearch
 
 class ViewController: UIViewController {
 
+    let ALGOLIA_APP_ID = "latency"
+    let ALGOLIA_INDEX_NAME = "bestbuy_promo"
+    let ALGOLIA_API_KEY = "1f6fd3a6fb973cb08419fe7d288fa4db"
+    var searcher: Searcher!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let client = Client(appID: ALGOLIA_APP_ID, apiKey: ALGOLIA_API_KEY)
+        let index = client.index(withName: ALGOLIA_INDEX_NAME)
+        searcher = Searcher(index: index, resultHandler: self.handleResults)
+        
+        
+        searcher.params.query = "iphone"
+        searcher.search()
+    }
+    
+    func handleResults(results: SearchResults?, error: Error?) {
+        guard let results = results else { return }
+        print(results.page)
+        print(results.hits)
     }
 
     override func didReceiveMemoryWarning() {
