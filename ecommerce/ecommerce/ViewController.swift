c1eff4d24bcdd6f6410d0fb8eb5d694f7697de06
Guy Daher <guy.daher@algolia.com>
2017-02-02 18:39:54 +0100

    Implementation of a customSearchBar (instantSearchBar) with customSearchController (instantSearchController), linked to a viewController with dummy country data.

--- ecommerce/ecommerce/ViewController.swift
+++ ecommerce/ecommerce/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 import InstantSearchCore
 import AlgoliaSearch
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating, UISearchBarDelegate, InstantSearchControllerDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var dataArray = [String]()
+    
+    var filteredArray = [String]()
+    
+    var shouldShowSearchResults = false
+    
+    var searchController: UISearchController!
+    
+    var instantSearchController: InstantSearchController!
+    
     let ALGOLIA_APP_ID = "latency"
     let ALGOLIA_INDEX_NAME = "bestbuy_promo"
     let ALGOLIA_API_KEY = Bundle.main.infoDictionary!["AlgoliaApiKey"] as! String
@@ -20,6 +32,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        loadListOfCountries()
+        
+        filteredArray = dataArray
+        
+        
+        // Uncomment the following line to enable the default search controller.
+        // configureSearchController()
+        
+        // Comment out the next line to disable the customized search controller and search bar and use the default ones. Also, uncomment the above line.
+        configureCustomSearchController()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         let client = Client(appID: ALGOLIA_APP_ID, apiKey: ALGOLIA_API_KEY)
         let index = client.index(withName: ALGOLIA_INDEX_NAME)
         searcher = Searcher(index: index, resultHandler: self.handleResults)
@@ -34,12 +63,173 @@ class ViewController: UIViewController {
         print(results.page)
         print(results.hits)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    // MARK: UITableView Delegate and Datasource functions
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if shouldShowSearchResults {
+            return filteredArray.count
+        }
+        else {
+            return dataArray.count
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "idCell", for: indexPath)
+        
+        if shouldShowSearchResults {
+            cell.textLabel?.text = filteredArray[indexPath.row]
+        }
+        else {
+            cell.textLabel?.text = dataArray[indexPath.row]
+        }
+        
+        cell.textLabel?.textColor = UIColor.white
+        cell.backgroundColor = UIColor(red: 44.0 / 255.0, green: 98.0 / 255.0, blue: 135.0 / 255.0, alpha: 1)
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+    
+    
+    // MARK: Custom functions
+    
+    func loadListOfCountries() {
+        // Specify the path to the countries list file.
+        let pathToFile = Bundle.main.path(forResource: "countries", ofType: "txt")
+        
+        if let path = pathToFile {
+            // Load the file contents as a string.
+            let countriesString = try! String(contentsOfFile: path, encoding: String.Encoding.utf8)
+            
+            // Append the countries from the string to the dataArray array by breaking them using the line change character.
+            dataArray = countriesString.components(separatedBy: "\n")
+            
+            // Reload the tableview.
+            tableView.reloadData()
+        }
+    }
+    
+    func configureSearchController() {
+        // Initialize and perform a minimum configuration to the search controller.
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search here..."
+        searchController.searchBar.delegate = self
+        searchController.searchBar.sizeToFit()
+        
+        // Place the search bar view to the tableview headerview.
+        tableView.tableHeaderView = searchController.searchBar
+    }
+    
+    
+    func configureCustomSearchController() {
+        
+        let backgroundColorOfSearchBar = UIColor(red: 13.0 / 255.0, green: 24.0 / 255.0, blue: 37.0 / 255.0, alpha: 1)
+        
+        instantSearchController = InstantSearchController(searchResultsController: self, searchBarFrame: CGRect(x: 0.0, y: 0.0, width: tableView.frame.size.width, height: 50.0), searchBarFont: UIFont(name: "Futura", size: 16.0)!, searchBarTextColor: UIColor.white, searchBarTintColor: backgroundColorOfSearchBar)
+        
+        instantSearchController.instantSearchBar.placeholder = "Search for movies"
+        tableView.tableHeaderView = instantSearchController.instantSearchBar
+        
+        instantSearchController.instantSearchControllerDelegate = self
+    }
+    
+    
+    // MARK: UISearchBarDelegate functions
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        shouldShowSearchResults = true
+        tableView.reloadData()
+    }
+    
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        shouldShowSearchResults = false
+        tableView.reloadData()
+    }
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if !shouldShowSearchResults {
+            shouldShowSearchResults = true
+            tableView.reloadData()
+        }
+        
+        searchController.searchBar.resignFirstResponder()
+    }
+    
+    
+    // MARK: UISearchResultsUpdating delegate function
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchString = searchController.searchBar.text else {
+            return
+        }
+        
+        // Filter the data array and get only those countries that match the search text.
+        filteredArray = dataArray.filter({ (country) -> Bool in
+            let countryText:NSString = country as NSString
+            
+            return (countryText.range(of: searchString, options: NSString.CompareOptions.caseInsensitive).location) != NSNotFound
+        })
+        
+        // Reload the tableview.
+        tableView.reloadData()
+    }
+    
+    
+    // MARK: CustomSearchControllerDelegate functions
+    
+    func didStartSearching() {
+        shouldShowSearchResults = true
+        tableView.reloadData()
+    }
+    
+    
+    func didTapOnSearchButton() {
+        if !shouldShowSearchResults {
+            shouldShowSearchResults = true
+            tableView.reloadData()
+        }
+    }
+    
+    
+    func didTapOnCancelButton() {
+        shouldShowSearchResults = false
+        tableView.reloadData()
+    }
+    
+    
+    func didChangeSearchText(_ searchText: String) {
+        
+        // Filter the data array and get only those countries that match the search text.
+        filteredArray = searchText.isEmpty ? dataArray : dataArray.filter({ (country) -> Bool in
+            let countryText: NSString = country as NSString
+            
+            return (countryText.range(of: searchText, options: NSString.CompareOptions.caseInsensitive).location) != NSNotFound
+        })
+        
+        // Reload the tableview.
+        tableView.reloadData()
+    }
 }
 
