9136319b43345fc7ff1202bb40689c7c99a374ab
Guy Daher <guy.daher@algolia.com>
2017-02-15 18:32:07 +0100

    Adding the views for many filters: Price, discount, shipping, number of reviews.

--- ecommerce/ecommerce/FilterViewController.swift
+++ ecommerce/ecommerce/FilterViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Eureka
 
 class FilterViewController: FormViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,20 +20,43 @@ class FilterViewController: FormViewController {
         navigationItem.rightBarButtonItem?.target = self
         navigationItem.rightBarButtonItem?.action = #selector(searchClicked(_:))
         
-        form +++ Section("Section1")
-            <<< TextRow() { row in
-                row.title = "Row Title"
-                row.placeholder = "Enter text here"
+        form +++ Section("Original Price")
+            <<< SliderRow { slider in
+                slider.title = "Minimum Price"
+                slider.value = 1
+                slider.minimumValue = 1
+                slider.maximumValue = 100 // TODO: Set correct values
             }
-            <<< PhoneRow() {
-                $0.title = "Phone Row"
-                $0.placeholder = "Add numbers here"
+            <<< SliderRow() { slider in
+                slider.title = "Maximum Price"
+                slider.value = 100
+                slider.maximumValue = 1
+                slider.maximumValue = 100
             }
-            +++ Section("Sectoin2")
-            <<< DateRow() {
-                $0.title = "Date Riw"
-                $0.value = Date(timeIntervalSinceReferenceDate: 0)
-        }
+            +++ Section("Promoted Price")
+            <<< StepperRow { stepper in
+                stepper.title = "Minimum Price"
+                stepper.value = 1
+            }
+            <<< StepperRow() { stepper in
+                stepper.title = "Maximum Price"
+                stepper.value = 100
+            }
+            +++ Section("Perks")
+            <<< CheckRow() { check in
+                check.title = "Has Discount?"
+                check.value = false
+            }
+            <<< SwitchRow() { check in
+                check.title = "Free Shipping?"
+                check.value = false
+            }
+            +++ Section("Quality")
+            <<< IntRow() { intRow in
+                intRow.title = "Minimum number of reviews"
+                intRow.value = 0
+            }
+        
     }
     
     func cancelClicked(_ barButtonItem: UIBarButtonItem) {
@@ -44,22 +67,22 @@ class FilterViewController: FormViewController {
         // TODO: Save the filters here before exiting!
         navigationController?.dismiss(animated: true, completion: nil)
     }
-
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
