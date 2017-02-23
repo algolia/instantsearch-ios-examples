f5f15c14c8cb2c84692381011f445e6e1d10db85
Guy Daher <guy.daher@algolia.com>
2017-02-23 17:59:04 +0100

    Create FilterVC, and then have the boilerplate code to have uictonrols in accessoryView

--- ecommerce/ecommerce/FilterViewController.swift
+++ ecommerce/ecommerce/FilterViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class FilterViewController: UIViewController {
+class FilterViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
+        automaticallyAdjustsScrollViewInsets = false
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +26,27 @@ class FilterViewController: UIViewController {
     }
     
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "filterCell", for: indexPath)
+        
+        cell.textLabel?.text = "Hi"
+        
+        let mySwitch = UISlider(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        mySwitch.maximumValue = 50
+        mySwitch.minimumValue = 0
+        mySwitch.value = 25
+        var control = UIControl()
+        control = mySwitch
+        cell.accessoryView = control
+        
+        return cell
+    }
+    
+    
     /*
     // MARK: - Navigation
 
