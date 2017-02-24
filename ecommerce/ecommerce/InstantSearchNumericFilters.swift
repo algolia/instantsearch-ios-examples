88d3aed24498462d2ffaae2b7299b5cf031966fe
Guy Daher <guy.daher@algolia.com>
2017-02-24 11:21:31 +0100

    Created derived classes of 5 basic uicontrol to store info such as filter name and operation.

--- ecommerce/ecommerce/InstantSearchNumericFilters.swift
+++ ecommerce/ecommerce/InstantSearchNumericFilters.swift
@@ -7,12 +7,47 @@
 //
 
 import Foundation
+import InstantSearchCore
 import UIKit
+
+protocol InstantSearchNumericFilter {
+    var filterName: String? { get set }
+    var inclusive: Bool { get set }
+}
+
+class InstantSearchSlider : UISlider, InstantSearchNumericFilter {
+    var filterName: String?
+    var op: NumericRefinement.Operator?
+    var inclusive: Bool = true
+}
+
+class InstantSearchStepper : UIStepper, InstantSearchNumericFilter {
+    var filterName: String?
+    var op: NumericRefinement.Operator?
+    var inclusive: Bool = true
+}
+
+class InstantSearchSwitch : UISwitch, InstantSearchNumericFilter {
+    var filterName: String?
+    var inclusive: Bool = true
+}
+
+class InstantSearchSegmentedControl : UISegmentedControl, InstantSearchNumericFilter {
+    var filterName: String?
+    var op: NumericRefinement.Operator?
+    var inclusive: Bool = true
+}
+
+class InstantSearchDatePicker : UIDatePicker, InstantSearchNumericFilter {
+    var filterName: String?
+    var op: NumericRefinement.Operator?
+    var inclusive: Bool = true
+}
+
 
 extension InstantSearch {
     
     func addWidget(numericFilter: UIControl, forRefinement name: String) {
-        //numericFilters[name] = NumericFilter.uiControl(numericFilter)
         numericFilter.addTarget(self, action: #selector(numericFilterValueChanged(sender:)), for: .valueChanged)
         reloadAllWidgets()
     }
@@ -20,15 +55,15 @@ extension InstantSearch {
     // TODO: Change the filter value and trigger a search! 
     internal func numericFilterValueChanged(sender: UIControl) {
         switch sender {
-        case let slider as UISlider:
+        case let slider as InstantSearchSlider:
             print(slider.value)
-        case let mySwitch as UISwitch:
+        case let mySwitch as InstantSearchSwitch:
             print(mySwitch.isOn)
-        case let stepper as UIStepper:
+        case let stepper as InstantSearchStepper:
             print(stepper.value)
-        case let segmentedControl as UISegmentedControl:
+        case let segmentedControl as InstantSearchSegmentedControl:
             print(segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex)!)
-        case let datePicker as UIDatePicker:
+        case let datePicker as InstantSearchDatePicker:
             print(datePicker.date.timeIntervalSince1970)
         default: print("none!")
         }
