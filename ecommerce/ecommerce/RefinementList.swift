2373ed0a2ae3f6a75625c40ed59476d48a3f427d
Guy Daher <guy.daher@algolia.com>
2017-03-08 14:54:09 +0100

    Create Stats Widget with Templating logic. It implements both AlgoliaWidget and SearchProgressDelegate

---
+++ ecommerce/ecommerce/RefinementList.swift
@@ -0,0 +1,9 @@
+//
+//  RefinementList.swift
+//  ecommerce
+//
+//  Created by Guy Daher on 08/03/2017.
+//  Copyright © 2017 Guy Daher. All rights reserved.
+//
+
+import Foundation
---
+++ ecommerce/ecommerce/Stats.swift
@@ -0,0 +1,56 @@
+//
+//  Stats.swift
+//  ecommerce
+//
+//  Created by Guy Daher on 08/03/2017.
+//  Copyright © 2017 Guy Daher. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import InstantSearchCore
+
+@objc class Stats: UILabel, AlgoliaWidget, SearchProgressDelegate {
+    
+    var searcher: Searcher?
+    var searchProgressController: SearchProgressController?
+    
+    public var resultTemplate: String?
+    public var errorTemplate: String?
+    
+    private let defaultResultTemplate = "{nbHits} results found in {processingTimeMS} ms"
+    
+    // MARK: - AlgoliaWidget
+    
+    func initWith(searcher: Searcher) {
+        self.searcher = searcher
+        self.resultTemplate = defaultResultTemplate
+        
+        searchProgressController = SearchProgressController(searcher: searcher)
+        searchProgressController?.graceDelay = 0.01
+        searchProgressController?.delegate = self
+    }
+    
+    func on(results: SearchResults?, error: Error?, userInfo: [String: Any]) {
+        text = resultTemplate?.replacingOccurrences(of: "{hitsPerPage}", with: "\(results?.hitsPerPage)")
+            .replacingOccurrences(of: "{processingTimeMS}", with: "\(results?.processingTimeMS)")
+            .replacingOccurrences(of: "{nbHits}", with: "\(results?.nbHits)")
+            .replacingOccurrences(of: "{nbPages}", with: "\(results?.nbPages)")
+            .replacingOccurrences(of: "{page}", with: "\(results?.page)")
+            .replacingOccurrences(of: "{query}", with: "\(results?.query)")
+    }
+    
+    func onReset() {
+        text = "GONE"
+    }
+    
+    // MARK: - SearchProgressDelegate
+    
+    func searchDidStart(_ searchProgressController: SearchProgressController) {
+        text = "Hey I am searching"
+    }
+    
+    func searchDidStop(_ searchProgressController: SearchProgressController) {
+        
+    }
+}
