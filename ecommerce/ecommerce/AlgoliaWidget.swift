73bdb7ca964f717cb72458395bb68b2537616aab
Guy Daher <guy.daher@algolia.com>
2017-03-08 10:44:41 +0100

    Add AlgoliaWidget protocol and RefinementControlWidget, will be used by the InstantSearch ViewModel to coordinate everything

---
+++ ecommerce/ecommerce/AlgoliaWidget.swift
@@ -0,0 +1,21 @@
+//
+//  AlgoliaWidget.swift
+//  ecommerce
+//
+//  Created by Guy Daher on 08/03/2017.
+//  Copyright © 2017 Guy Daher. All rights reserved.
+//
+
+import Foundation
+import InstantSearchCore
+
+@objc protocol AlgoliaWidget {
+    @objc func initWith(searcher: Searcher)
+    @objc func on(results: SearchResults?, error: Error?, userInfo: [String: Any])
+    @objc func onReset()
+}
+
+@objc protocol RefinementControlWidget: AlgoliaWidget {
+    @objc func registerValueChangedAction()
+    @objc func onRefinementChange(numerics: [String: [NumericRefinement]], facets: [String: [FacetRefinement]])
+}
