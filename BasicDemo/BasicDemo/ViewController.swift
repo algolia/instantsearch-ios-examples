cb6b72dd32c364760f369ad2a1e19f15d46501a8
Guy Daher <guy.daher@algolia.com>
2017-03-22 10:20:59 +0100

    Create BasicDemo Project

---
+++ BasicDemo/BasicDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicDemo
+//
+//  Created by Guy Daher on 22/03/2017.
+//  Copyright © 2017 Guy Daher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
