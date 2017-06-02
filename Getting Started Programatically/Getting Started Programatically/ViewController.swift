525d362dedfb338e965337a2538859ce92db10bd
Guy Daher <guy.daher@algolia.com>
2017-06-02 15:49:36 +0200

    demo(guide): organise the code better to make it easier to digest in the getting started guide

--- Getting Started Programatically/Getting Started Programatically/ViewController.swift
+++ Getting Started Programatically/Getting Started Programatically/ViewController.swift
@@ -11,32 +11,16 @@ import InstantSearch
 
 class ViewController: HitsTableViewController {
     
+    let searchBar = SearchBarWidget(frame: .zero)
+    let stats = StatsLabelWidget(frame: .zero)
+    let tableView = HitsTableWidget(frame: .zero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let searchBar = SearchBarWidget(frame: .zero)
-        let stats = StatsLabelWidget(frame: .zero)
-        let tableView = HitsTableWidget(frame: .zero)
-        searchBar.translatesAutoresizingMaskIntoConstraints = false
-        stats.translatesAutoresizingMaskIntoConstraints = false
-        tableView.translatesAutoresizingMaskIntoConstraints = false
+        initUI()
         
-        self.view.addSubview(searchBar)
-        self.view.addSubview(stats)
-        self.view.addSubview(tableView)
-        
-        let views = ["searchBar": searchBar, "stats": stats, "tableView": tableView]
-        
-        var constraints = [NSLayoutConstraint]()
-        constraints += NSLayoutConstraint.constraints(withVisualFormat: "V:|-30-[searchBar]-10-[stats]-10-[tableView]-|", options: [], metrics:
-            nil, views:views)
-        constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-25-[searchBar]-25-|", options: [], metrics: nil, views:views)
-        constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-25-[stats]-25-|", options: [], metrics: nil, views:views)
-        constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-[tableView]-|", options: [], metrics: nil, views:views)
-        
-        NSLayoutConstraint.activate(constraints)
-        
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "hitCell")
+        // and assign tableView to InstantSearch
         hitsTableView = tableView
         
         // Add all widgets to InstantSearch
@@ -52,11 +36,30 @@ class ViewController: HitsTableViewController {
         return cell
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func initUI() {
+        // Create SearchBar, Stats and TableView
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        stats.translatesAutoresizingMaskIntoConstraints = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add them to the main view
+        self.view.addSubview(searchBar)
+        self.view.addSubview(stats)
+        self.view.addSubview(tableView)
+        
+        // Add autolayour constraints
+        let views = ["searchBar": searchBar, "stats": stats, "tableView": tableView]
+        var constraints = [NSLayoutConstraint]()
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "V:|-30-[searchBar]-10-[stats]-10-[tableView]-|", options: [], metrics:
+            nil, views:views)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-25-[searchBar]-25-|", options: [], metrics: nil, views:views)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-25-[stats]-25-|", options: [], metrics: nil, views:views)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-[tableView]-|", options: [], metrics: nil, views:views)
+        NSLayoutConstraint.activate(constraints)
+        
+        // Register tableView identifier
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "hitCell")
     }
-    
     
 }
 
