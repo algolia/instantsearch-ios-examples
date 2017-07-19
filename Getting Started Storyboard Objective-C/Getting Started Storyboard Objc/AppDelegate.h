1a54bd1ad1b555c8be7025d3eda7acbe9d02e5c8
Guy Daher <guy.daher@algolia.com>
2017-07-19 15:01:26 +0200

    demo(objc): add objective-c demo

---
+++ Getting Started Storyboard Objective-C/Getting Started Storyboard Objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Getting Started Storyboard Objc
+//
+//  Created by Guy Daher on 23/06/2017.
+//  Copyright © 2017 Guy Daher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
