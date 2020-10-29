f8d099e5472d4f16d4e9baefe258406a668d5329
Vladislav Fitc <vladislav.fitz@gmail.com>
2020-10-29 16:30:26 +0100

    Improve numeric range demo

--- DemoDirectory/DemoDirectory/Helpers/NumericRangeController.swift
+++ DemoDirectory/DemoDirectory/Helpers/NumericRangeController.swift
@@ -10,7 +10,7 @@ import Foundation
 import InstantSearchCore
 import UIKit
 
-public class NumericRangeController: NumberRangeController {
+public class NumericRangeController: UIViewController, NumberRangeController {
   public var onRangeChanged: ((ClosedRange<Double>) -> Void)?
 
   public typealias Number = Double
@@ -28,13 +28,52 @@ public class NumericRangeController: NumberRangeController {
     rangerSlider.minimumValue = bounds.lowerBound
     rangerSlider.maximumValue = bounds.upperBound
     setItem(bounds)
+    lowerBoundLabel.text = "\(bounds.lowerBound.rounded(toPlaces: 2))"
+    upperBoundLabel.text = "\(bounds.upperBound.rounded(toPlaces: 2))"
   }
 
+  
+  public let lowerBoundLabel: UILabel
+  public let upperBoundLabel: UILabel
   public let rangerSlider: RangeSlider
 
   public init(rangeSlider: RangeSlider) {
     self.rangerSlider = rangeSlider
+    self.lowerBoundLabel = .init()
+    self.upperBoundLabel = .init()
+    super.init(nibName: nil, bundle: nil)
     rangeSlider.addTarget(self, action: #selector(onValueChanged(sender:)), for: [.touchUpInside, .touchUpOutside])
   }
-
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    rangerSlider.translatesAutoresizingMaskIntoConstraints = false
+    
+    lowerBoundLabel.translatesAutoresizingMaskIntoConstraints = false
+    lowerBoundLabel.widthAnchor.constraint(equalToConstant: 50).isActive = true
+    lowerBoundLabel.textAlignment = .center
+    upperBoundLabel.translatesAutoresizingMaskIntoConstraints = false
+    upperBoundLabel.widthAnchor.constraint(equalToConstant: 50).isActive = true
+    
+    rangerSlider.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    rangerSlider.widthAnchor.constraint(equalToConstant: 500).isActive = true
+        
+    upperBoundLabel.textAlignment = .center
+    let stackView = UIStackView()
+      .set(\.axis, to: .horizontal)
+      .set(\.spacing, to: .px16)
+      .set(\.distribution, to: .equalSpacing)
+      .set(\.translatesAutoresizingMaskIntoConstraints, to: false)
+      .set(\.alignment, to: .center)
+    stackView.addArrangedSubview(lowerBoundLabel)
+    stackView.addArrangedSubview(rangerSlider)
+    stackView.addArrangedSubview(upperBoundLabel)
+    view.addSubview(stackView)
+    stackView.pin(to: view)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
 }
