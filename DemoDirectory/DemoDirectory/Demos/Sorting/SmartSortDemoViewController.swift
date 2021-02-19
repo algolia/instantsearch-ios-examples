a7d02493995f5e0ecffe025ec638bfd65d61930d
Vladislav Fitc <vladislav.fitz@gmail.com>
2021-02-19 16:56:53 +0100

    Improve smart sort demo

--- DemoDirectory/DemoDirectory/Demos/Sorting/SmartSortDemoViewController.swift
+++ DemoDirectory/DemoDirectory/Demos/Sorting/SmartSortDemoViewController.swift
@@ -10,48 +10,42 @@ import Foundation
 import UIKit
 import InstantSearch
 
-class SmartSortDemoViewController: UIViewController, SelectableSegmentController {
-    
-  typealias HitType = ShopItem
-  
+class SmartSortDemoViewController: UIViewController {
+      
   let indices: [IndexName] = [
     "test_Bestbuy", // default
     "test_Bestbuy_vr_price_asc", //smart sort
     "test_Bestbuy_replica_price_asc", //replica
   ]
-  
-  private let cellIdentifier = "CellID"
-  
-  let searchBar: UISearchBar
-  
+    
   let searcher: SingleIndexSearcher
   
+  let searchBar: UISearchBar
   let queryInputConnector: QueryInputConnector
   let textFieldController: TextFieldController
   
-  let hitsConnector: HitsConnector<HitType>
-  let hitsTableViewController: ResultsTableViewController
+  let indexSegmentInteractor: IndexSegmentInteractor
+  var onClick: ((Int) -> Void)?
   
   let statsConnector: StatsConnector
   let statsController: LabelStatsController
   
-  let mainStackView = UIStackView(frame: .zero)
-  
-  let smartSortToggleView: SmartSortToggleControl
   let smartSortConnector: SmartSortConnector
+  let smartSortController: ButtonSmartSortController
   
-  let indexSegmentInteractor: IndexSegmentInteractor
-  let indexSegmentControl: UISegmentedControl
-  var onClick: ((Int) -> Void)?
-  
-  
+  let hitsConnector: HitsConnector<ShopItem>
+  let hitsTableViewController: ResultsTableViewController
+        
   override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-    searcher = .init(appID: "C7RIRJRYR9", apiKey: "77af6d5ffb27caa5ff4937099fcb92e8", indexName: "test_Bestbuy_vr_price_asc")
+    searcher = .init(appID: "C7RIRJRYR9",
+                     apiKey: "77af6d5ffb27caa5ff4937099fcb92e8",
+                     indexName: "test_Bestbuy_vr_price_asc")
     searchBar = .init()
-    smartSortToggleView = .init()
+    smartSortController = .init()
+    searchBar.showsScopeBar = true
     statsController = .init()
     statsConnector = .init(searcher: searcher, controller: statsController)
-    smartSortConnector = .init(searcher: searcher, controller: smartSortToggleView)
+    smartSortConnector = .init(searcher: searcher, controller: smartSortController)
     textFieldController = .init(searchBar: searchBar)
     queryInputConnector = .init(searcher: searcher, controller: textFieldController)
 
@@ -60,11 +54,9 @@ class SmartSortDemoViewController: UIViewController, SelectableSegmentController
     
     let items: [Int: Index] = .init(uniqueKeysWithValues: indices.map(searcher.client.index(withName:)).enumerated().map { ($0.offset, $0.element) })
     indexSegmentInteractor = .init(items: items, selected: 0)
-    indexSegmentControl = .init()
 
     super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         
-    indexSegmentControl.addTarget(self, action: #selector(didSelectSegment), for: .valueChanged)
     indexSegmentInteractor.connectController(self) { index in
       switch index.name {
       case "test_Bestbuy": return "Most relevant"
@@ -96,24 +88,22 @@ class SmartSortDemoViewController: UIViewController, SelectableSegmentController
     
     searchBar.translatesAutoresizingMaskIntoConstraints = false
     searchBar.searchBarStyle = .minimal
+    searchBar.delegate = self
     
     let stackView = UIStackView()
-      .set(\.spacing, to: .px16)
+      .set(\.spacing, to: 7)
       .set(\.axis, to: .vertical)
       .set(\.translatesAutoresizingMaskIntoConstraints, to: false)
     stackView.addArrangedSubview(searchBar)
-    indexSegmentControl.translatesAutoresizingMaskIntoConstraints = false
-    stackView.addArrangedSubview(indexSegmentControl)
     statsController.label.translatesAutoresizingMaskIntoConstraints = false
     statsController.label.numberOfLines = 0
     statsController.label.textAlignment = .center
     stackView.addArrangedSubview(statsController.label)
-    smartSortToggleView.translatesAutoresizingMaskIntoConstraints = false
-    stackView.addArrangedSubview(smartSortToggleView)
+    smartSortController.view.translatesAutoresizingMaskIntoConstraints = false
+    stackView.addArrangedSubview(smartSortController.view)
     stackView.addArrangedSubview(hitsTableViewController.view)
     
     view.addSubview(stackView)
-
     NSLayoutConstraint.activate([
       stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
       stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
@@ -122,21 +112,24 @@ class SmartSortDemoViewController: UIViewController, SelectableSegmentController
     ])
   }
   
-  func configureMainStackView() {
-    mainStackView.axis = .vertical
-    mainStackView.spacing = .px16
-    mainStackView.distribution = .fill
-    mainStackView.translatesAutoresizingMaskIntoConstraints = false
-    mainStackView.alignment = .center
-  }
+}
+
+extension SmartSortDemoViewController: SelectableSegmentController {
   
   func setSelected(_ selected: Int?) {
-    indexSegmentControl.selectedSegmentIndex = selected ?? 0
+    searchBar.selectedScopeButtonIndex = selected ?? 0
   }
   
   func setItems(items: [Int : String]) {
-    indexSegmentControl.removeAllSegments()
-    items.forEach { indexSegmentControl.insertSegment(withTitle: $0.value, at: $0.key, animated: false) }
+    searchBar.scopeButtonTitles = items.sorted(by:\.key).map(\.value)
+  }
+  
+}
+
+extension SmartSortDemoViewController: UISearchBarDelegate {
+  
+  func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+    onClick?(selectedScope)
   }
   
 }
