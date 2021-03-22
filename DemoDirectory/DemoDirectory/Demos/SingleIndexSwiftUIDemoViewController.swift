d943210ac84d38cabf313cef2205a504241f4a38
Vladislav Fitc <vladislav.fitz@gmail.com>
2021-03-22 15:38:42 +0100

    wip

--- DemoDirectory/DemoDirectory/Demos/SingleIndexSwiftUIDemoViewController.swift
+++ DemoDirectory/DemoDirectory/Demos/SingleIndexSwiftUIDemoViewController.swift
@@ -18,69 +18,60 @@ struct ContentView: View {
     
     var body: some View {
       return VStack(spacing: 10) {
-        SearchBar(text: viewModel.queryBinding)
+        HStack(alignment: .center, spacing: 5) {
+          SearchBar(text: $viewModel.query)
+        }.padding(.horizontal, 5)
         Text(viewModel.hitsCountDescription)
           .fontWeight(.medium)
-        HitsView<ShopItemRow>(hitsCount: $viewModel.hitsCount,
-                 lastAppear: viewModel.lastRowIndexBinding,
-                 fetcher: viewModel.hitsInteractor.hit(atIndex:))
+        HitsView<ShopItemRow>(hitsInteractor: viewModel.hitsInteractor)
       }.onAppear {
         self.viewModel.searcher.search()
       }
+      .navigationBarTitle("Algolia & SwiftUI")
+      .navigationBarItems(trailing: Button(action: {
+        
+      }) {
+        Image(systemName: "line.horizontal.3.decrease.circle").font(.title)
+      }
+      )
     }
 }
 
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-       ContentView()
+      NavigationView {
+        ContentView()
+      }
     }
 }
 
 class AlgoliaViewModel: ObservableObject {
 
   @Published var hitsCountDescription: String = ""
-  @Published var hitsCount: Int = 0
-  var hitFetcher: ((Int) -> SUIShopItem?)
-
-  let queryBinding: Binding<String>
-  let lastRowIndexBinding: Binding<Int>
+  @Published var query: String = "" {
+    didSet {
+      searcher.query = query
+      searcher.search()
+    }
+  }
   
   let searcher: SingleIndexSearcher
   let hitsInteractor: HitsInteractor<SUIShopItem>
   let statsInteractor: StatsInteractor
-    
+  var queryInputInteractor: QueryInputInteractor
+  
   init() {
     let searcher = SingleIndexSearcher(appID: "latency",
     apiKey: "1f6fd3a6fb973cb08419fe7d288fa4db",
     indexName: "bestbuy")
     let hitsInteractor = HitsInteractor<SUIShopItem>(infiniteScrolling: .on(withOffset: 20), showItemsOnEmptyQuery: true)
-    hitsInteractor.connectSearcher(searcher)
     self.searcher = searcher
+    self.queryInputInteractor = .init()
     self.statsInteractor = .init()
     self.hitsInteractor = hitsInteractor
     searcher.indexQueryState.query = Query()
-    hitFetcher = hitsInteractor.hit(atIndex:)
-    queryBinding = .init(
-      get: {
-        searcher.query ?? ""
-      },
-      set: {
-        searcher.query = $0
-        searcher.search()
-      }
-    )
-    lastRowIndexBinding = .init(
-      get: { 0 },
-      set: {
-        hitsInteractor.notifyForInfiniteScrolling(rowNumber: $0)
-      }
-    )
-    searcher.onResults.subscribe(with: self) { (viewModel, response) in
-      print("Received result for query \(response.query), page \(response.page), hits: \(response.hits.count)")
-    }
-    hitsInteractor.onResultsUpdated.subscribe(with: self) { (viewModel, results) in
-      viewModel.hitsCount = viewModel.hitsInteractor.numberOfHits()
-    }.onQueue(.main)
+    hitsInteractor.connectSearcher(searcher)
+    queryInputInteractor.connectSearcher(searcher)
     statsInteractor.connectSearcher(searcher)
     statsInteractor.onItemChanged.subscribe(with: self) { (viewModel, stats) in
       if let stats = stats {
@@ -105,9 +96,12 @@ struct ShopItemRow: HitRow {
   let subtitle: String
   let details: String
   let imageURL: URL
+  let index: Int
 
   var body: some View {
     HStack(alignment: .center, spacing: 20) {
+      Text("\(index)")
+        .fontWeight(.light)
       WebImage(url: imageURL)
         .resizable()
         .indicator(.activity)
@@ -131,13 +125,15 @@ struct ShopItemRow: HitRow {
     self.subtitle = ""
     self.details = ""
     self.imageURL = URL(string: "google.com")!
+    self.index = 0
   }
   
-  init(item: SUIShopItem?) {
+  init(item: SUIShopItem?, index: Int) {
     self.title = item?.name ?? ""
     self.subtitle = item?.manufacturer ?? ""
     self.details = ""
     self.imageURL = item?.image ?? URL(string: "google.com")!
+    self.index = index
   }
 }
 
@@ -145,21 +141,36 @@ protocol HitRow: View {
   
   associatedtype Item
 
-  init(item: Item?)
+  init(item: Item?, index: Int)
 }
 
-struct HitsView<Row: HitRow>: View {
+struct HitsView<Row: HitRow>: View where Row.Item: Codable {
   
-  @Binding var hitsCount: Int
-  @Binding var lastAppear: Int
-  var fetcher: (Int) -> Row.Item?
-  
+  @ObservedObject var hi: HitsInteractor<Row.Item>
+        
   var body: some View {
-    List(0..<hitsCount, id: \.self) { index in
-      Row(item: fetcher(index)).onAppear {
-       lastAppear = index
+    if #available(iOS 14.0, *) {
+      ScrollView(showsIndicators: false) {
+        LazyVStack() {
+          ForEach(0..<hi.numberOfHits(), id: \.self) { index in
+            Row(item: hi.hit(atIndex: index), index: index).onAppear {
+              hi.notifyForInfiniteScrolling(rowNumber: index)
+            }
+          }
+        }
+      }
+    } else {
+      List(0..<hi.numberOfHits(), id: \.self) { index in
+        Row(item: hi.hit(atIndex: index), index: index).onAppear {
+          hi.notifyForInfiniteScrolling(rowNumber: index)
+        }
       }
     }
+  }
+  
+  init(hitsInteractor: HitsInteractor<Row.Item>) {
+    self.hi = hitsInteractor
+    UIScrollView.appearance().keyboardDismissMode = .interactive
   }
   
 }
@@ -171,48 +182,57 @@ struct SearchBar: View {
 
   var body: some View {
     HStack {
-      TextField("Search ...", text: $text)
-            .padding(7)
-            .padding(.horizontal, 25)
-            .background(Color(.systemGray5))
-            .cornerRadius(8)
-            .overlay(
-                HStack {
-                    Image(systemName: "magnifyingglass")
-                        .foregroundColor(.gray)
-                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, 8)
-
-                    if isEditing {
-                        Button(action: {
-                            self.text = ""
-                        }) {
-                            Image(systemName: "multiply.circle.fill")
-                                .foregroundColor(.gray)
-                                .padding(.trailing, 8)
-                        }
-                    }
+      ZStack {
+        TextField("Search ...", text: $text)
+          .padding(7)
+          .padding(.horizontal, 25)
+          .background(Color(.systemGray5))
+          .cornerRadius(8)
+          .overlay(
+            HStack {
+              Image(systemName: "magnifyingglass")
+                .foregroundColor(.gray)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 8)
+                .disabled(true)
+              if isEditing && !text.isEmpty {
+                Button(action: {
+                  self.text = ""
+                }) {
+                  Image(systemName: "multiply.circle.fill")
+                    .foregroundColor(.gray)
+                    .padding(.trailing, 8)
                 }
-            )
-            .padding(.horizontal, 10)
-            .onTapGesture {
-                self.isEditing = true
+              }
             }
-         if isEditing {
-           Button(action: {
-               self.isEditing = false
-               self.text = ""
-           }) {
-               Text("Cancel")
-           }
-           .padding(.trailing, 10)
-           .transition(.move(edge: .trailing))
-           .animation(.default)
+          )
+          .padding(.horizontal, 10)
+          .onTapGesture {
+            self.isEditing = true
+          }
+      }
+      if isEditing {
+        Button(action: {
+          self.isEditing = false
+          self.text = ""
+          self.hideKeyboard()
+        }) {
+          Text("Cancel")
         }
+        .padding(.trailing, 10)
+        .transition(.move(edge: .trailing))
+        .animation(.default)
+      }
     }
     .background(Color(.white))
-    .padding(.top, 20)
-
   }
   
 }
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
