798e9774db5174b067c832e4eb7f38ca8b34a066
Vladislav Fitc <vladislav.fitz@gmail.com>
2021-03-29 18:49:27 +0200

    move declarative logic and ui components to InstantSearch

--- DemoDirectory/DemoDirectory/Demos/SwiftUI/SwiftUIDemoViewController.swift
+++ DemoDirectory/DemoDirectory/Demos/SwiftUI/SwiftUIDemoViewController.swift
@@ -9,6 +9,7 @@
 import Foundation
 import InstantSearch
 import SwiftUI
+import SDWebImageSwiftUI
 
 class SwiftUIDemoViewController: UIHostingController<ContentView> {
   
@@ -19,6 +20,7 @@ class SwiftUIDemoViewController: UIHostingController<ContentView> {
     super.init(rootView: contentView)
     viewModel.setup(contentView)
     viewModel.searcher.search()
+    UIScrollView.appearance().keyboardDismissMode = .interactive
   }
   
   @objc required dynamic init?(coder aDecoder: NSCoder) {
@@ -27,68 +29,100 @@ class SwiftUIDemoViewController: UIHostingController<ContentView> {
   
 }
 
-extension AlgoliaViewModel {
-  static let test = AlgoliaViewModel(appID: "latency",
-                                     apiKey: "1f6fd3a6fb973cb08419fe7d288fa4db",
-                                     indexName: "bestbuy",
-                                     facetAttribute: "manufacturer")
-}
-
 struct ContentView: View {
   
+  @ObservedObject var statsObservable: StatsObservableController
+  @ObservedObject var hitsObservable: HitsObservableController<SUIShopItem>
+  @ObservedObject var facetStorage: FacetListObservableController
+  @ObservedObject var currentFiltersObservable: CurrentFiltersObservableController
+  @ObservedObject var queryInputObservable: QueryInputObservableController
+  @ObservedObject var filterClearObservable: FilterClearObservable
+
   @State private var showFacets = false
   
-  @ObservedObject var statsObservable: StatsObservable = .init()
-  @ObservedObject var hitsObservable: HitsObservable<SUIShopItem> = .init()
-  @ObservedObject var facetStorage: FacetStorage = .init()
-  @ObservedObject var currentFiltersObservable: CurrentFiltersObservable = .init()
-  @ObservedObject var queryInputObservable: QueryInputObservable = .init()
+  init() {
+    statsObservable = .init()
+    hitsObservable = .init()
+    facetStorage = .init()
+    currentFiltersObservable = .init()
+    queryInputObservable = .init()
+    filterClearObservable = .init()
+  }
   
   var body: some View {
-    return VStack(spacing: 10) {
+    return VStack(spacing: 7) {
       SearchBar(text: $queryInputObservable.query)
       Text(statsObservable.stats)
         .fontWeight(.medium)
-      HitsView(hitsObservable) { (item, _) in
+      HitsList(hitsObservable) { (item, _) in
         ShopItemRow(item: item)
       } noResults: {
         Text("No Results")
-          .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
       }
     }
     .navigationBarTitle("Algolia & SwiftUI")
-    .navigationBarItems(trailing:
-                          Button(action: { withAnimation { showFacets.toggle() } },
-                                 label: {
-                                  if currentFiltersObservable.isEmpty {
-                                    Image(systemName: "line.horizontal.3.decrease.circle")
-                                      .font(.title)
-                                  } else {
-                                    Image(systemName: "line.horizontal.3.decrease.circle.fill")
-                                      .font(.title)
-                                  }
-                                 }).sheet(isPresented: $showFacets, content: {
-                                  NavigationView {
-                                    FacetsScreen(statsObservable: statsObservable,
-                                                 facetStorage: facetStorage)
-                                  }
-                                 })
-    )
+    .navigationBarItems(trailing: facetsButton())
   }
-}
+  
+  private func facetsButton() -> some View {
+    Button(action: { withAnimation { showFacets.toggle() } },
+           label: {
+            let imageName = currentFiltersObservable.isEmpty ? "line.horizontal.3.decrease.circle" : "line.horizontal.3.decrease.circle.fill"
+            Image(systemName: imageName)
+              .font(.title)
+           }).sheet(isPresented: $showFacets,
+                    content: facetsView)
 
-struct FacetsScreen: View {
+  }
   
-  @ObservedObject var statsObservable: StatsObservable
-  @ObservedObject var facetStorage: FacetStorage
-  
-  var body: some View {
-    VStack {
-      Text(statsObservable.stats)
-        .fontWeight(.medium)
-      FacetListView(facetStorage: facetStorage)
+  private func facetsView() -> some View {
+    NavigationView {
+      if #available(iOS 14.0, *) {
+        VStack {
+          FacetList(facetStorage) { facet, isSelected in
+            VStack {
+              FacetRow(facet: facet, isSelected: isSelected)
+              Divider()
+            }
+            .padding(.vertical, 7)
+            .background(Color(.systemBackground))
+          }
+        }
+        .navigationBarTitle("Manufacturer")
+        .toolbar {
+            ToolbarItem(placement: .bottomBar) {
+              Spacer()
+            }
+            ToolbarItem(placement: .bottomBar) {
+              Text(statsObservable.stats)
+            }
+            ToolbarItem(placement: .bottomBar) {
+              Spacer()
+            }
+            ToolbarItem(placement: .bottomBar) {
+              Button(action: filterClearObservable.clear,
+                     label: { Image(systemName: "trash") }
+              ).disabled(currentFiltersObservable.isEmpty)
+            }
+        }
+      } else {
+        VStack {
+          Text(statsObservable.stats)
+            .fontWeight(.medium)
+          FacetList(facetStorage) { facet, isSelected in
+            VStack {
+              FacetRow(facet: facet, isSelected: isSelected)
+                .background(Color(.green))
+              Divider()
+            }
+            .padding(.vertical, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            .background(Color(.systemBackground))
+          }
+        }
+        .navigationBarTitle("Manufacturer")
+      }
     }
-    .navigationBarTitle("Manufacturer")
   }
   
 }
@@ -105,7 +139,6 @@ struct ContentView_Previews : PreviewProvider {
     }.onAppear {
       viewModel.searcher.search()
     }
-    
   }
 }
 
@@ -123,6 +156,7 @@ class AlgoliaViewModel {
   let facetListInteractor: FacetListInteractor
   let currentFiltersInteractor: CurrentFiltersInteractor
   let filterState: FilterState
+  let filterClearInteractor: FilterClearInteractor
     
   init(appID: ApplicationID,
        apiKey: APIKey,
@@ -142,6 +176,7 @@ class AlgoliaViewModel {
     self.filterState = .init()
     self.currentFiltersInteractor = .init()
     self.queryInputInteractor = .init()
+    self.filterClearInteractor = .init()
     setupConnections()
   }
   
@@ -153,6 +188,9 @@ class AlgoliaViewModel {
     statsInteractor.connectSearcher(searcher)
     currentFiltersInteractor.connectFilterState(filterState)
     queryInputInteractor.connectSearcher(searcher)
+    filterClearInteractor.connectFilterState(filterState,
+                                             filterGroupIDs: [.or(name: facetAttribute.rawValue, filterType: .facet)],
+                                             clearMode: .specified)
   }
   
   func setup(_ contentView: ContentView) {
@@ -161,15 +199,66 @@ class AlgoliaViewModel {
     facetListInteractor.connectController(contentView.facetStorage, with: FacetListPresenter(sortBy: [.isRefined, .count(order: .descending)]))
     currentFiltersInteractor.connectController(contentView.currentFiltersObservable)
     queryInputInteractor.connectController(contentView.queryInputObservable)
+    filterClearInteractor.connectController(contentView.filterClearObservable)
   }
     
 }
 
-struct SUIShopItem: Codable, Hashable {
+extension AlgoliaViewModel {
+  static let test = AlgoliaViewModel(appID: "latency",
+                                     apiKey: "1f6fd3a6fb973cb08419fe7d288fa4db",
+                                     indexName: "bestbuy",
+                                     facetAttribute: "manufacturer")
+}
+
+struct ShopItemRow: View {
+  
+  let title: String
+  let subtitle: String
+  let details: String
+  let imageURL: URL
+
+  var body: some View {
+    VStack {
+      HStack(alignment: .center, spacing: 20) {
+        WebImage(url: imageURL)
+          .resizable()
+          .indicator(.activity)
+          .scaledToFit()
+          .clipped()
+          .frame(width: 100, height: 100, alignment: .leading)
+        VStack(alignment: .leading, spacing: 5) {
+          Text(title)
+            .fontWeight(.heavy)
+          Spacer()
+            .frame(height: 1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+          Text(subtitle)
+            .font(.system(size: 14, weight: .medium, design: .default))
+          Text(details)
+        }.multilineTextAlignment(.leading)
+      }
+      .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 100, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 140, maxHeight: 140, alignment: .leading)
+      .padding(.horizontal, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+      Divider()
+    }
+  }
+  
+  init(item: SUIShopItem?) {
+    self.title = item?.name ?? ""
+    self.subtitle = item?.manufacturer ?? ""
+    self.details = ""
+    self.imageURL = item?.image ?? URL(string: "google.com")!
+  }
+}
+
+struct SUIShopItem: Codable, Hashable, CustomStringConvertible {
   let objectID: String
   let name: String
   let manufacturer: String?
   let shortDescription: String?
   let image: URL?
+  
+  var description: String {
+    return name
+  }
 }
-
